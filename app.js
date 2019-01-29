a8fea169f7a04ed76edf83c916da6ec14c6db7dc
set bro <sbro@hotmail.com>
2019-01-29 10:10:07 -0500

    v1

---
+++ app.js
@@ -0,0 +1,47 @@
+var vmdl = new Vue({
+  el: "#app",
+  data: {
+    nav: 1,
+    tr: 0,
+    items: [
+      { name: "aa" },
+      { name: "bb" },
+      { name: "cc" },
+      { name: "dd" },
+      { name: "ee" },
+      { name: "ff" },
+      { name: "gg" },
+      { name: "hh" },
+      { name: "ii" },
+      { name: "jj" },
+      { name: "kk" },
+      { name: "ll" },
+      { name: "mm" },
+      { name: "nn" },
+      { name: "oo" },
+      { name: "pp" },
+      { name: "qq" },
+      { name: "rr" },
+      { name: "ss" },
+      { name: "tt" },
+      { name: "uu" },
+      { name: "vv" },
+      { name: "ww" },
+      { name: "xx" },
+      { name: "yy" },
+      { name: "zz" }
+    ]
+  },
+  computed: {
+    //reversedMessage: function() {}
+  },
+  methods: {
+    //navr: function() {}
+  },
+  created: function() {
+    //this.debouncedGetAnswer = _.debounce(this.getAnswer, 500);
+  },
+  watch: {
+    //question: function(newQuestion, oldQuestion) {}
+  }
+});
